76e6b8adad1a81e332330539f4aaa685578f19cb
Adam DeMuri <adam.demuri@gmail.com>
2023-03-19 19:02:21 -0600

    Add OTA support for ESP32

--- src/esp32/esp32.cc
+++ src/esp32/esp32.cc
@@ -2,6 +2,7 @@
 #include <Adafruit_SH110X.h>
 #include <Arduino.h>
 #include <ArduinoJson.h>
+#include <ArduinoOTA.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <ESPmDNS.h>
@@ -254,6 +255,37 @@ void setup() {
               received_size);
         }
       });
+  ArduinoOTA
+      .onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+          type = "sketch";
+        else  // U_SPIFFS
+          type = "filesystem";
+
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS
+        // using SPIFFS.end()
+        Serial.println("Start updating " + type);
+      })
+      .onEnd([]() { Serial.println("\nEnd"); })
+      .onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+      })
+      .onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR)
+          Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR)
+          Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR)
+          Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR)
+          Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR)
+          Serial.println("End Failed");
+      });
+  ArduinoOTA.begin();
+
   Serial.println(" done.");
 
   Serial.print("Initializing serial...");
@@ -275,6 +307,8 @@ void setup() {
 
 void loop() {
   websocket.cleanupClients();
+  ArduinoOTA.handle();
+
   bool do_reset = command.reset;
   if (transfer_in.receiveData()) {
     received_at = millis();
